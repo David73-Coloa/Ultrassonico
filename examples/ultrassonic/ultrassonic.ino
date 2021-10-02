12e162f85de70cd88ead6bb4947078cac0c7107c
David73-Coloa <76916389+David73-Coloa@users.noreply.github.com>
2021-10-02 17:22:46 -0300

    First commit

---
+++ examples/ultrassonic/ultrassonic.ino
@@ -0,0 +1,31 @@
+/*
+
+    Autor: David luighy Sardinha Mendes
+    Outubro de 2021
+    Feito para utilizar o sensor ultrassonico
+*/
+
+
+#include <D_ultrassonic.h>
+
+
+// Criação do objeto ultra (nome dado a um sesnsor ultrassônico), conectado aos pinos 2 e 3, (2 - pino Trigger, 3 - pino Echo)
+D_ultrassonic ultra(2, 3);
+
+
+float dist; // Declara uma variável do tipo float chamada dist
+
+
+void setup() {
+  Serial.begin(9600); // Inicializa a comunicação serial na velocidade 9600
+  
+}
+
+
+
+void loop() {
+  dist = ultra.ReturnDistanceInCm(); // A variável "dist" recebe o valor lido pelo "ultra" em centímetros, por meio do método ReturnDistanceInCm();
+  Serial.print(" Distancia: \n"); // Mostra " Distancia: " no monitor serial;
+  Serial.print(dist);               // E depois mostra o valor da variável dist;
+  delay(500); // Espera 500ms
+}
